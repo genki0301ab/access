3215585022fffbf307c17d8b9d3b1cc5ed901c16
岡田元輝 <genki0301ab@gmail.com>
2019-01-31 22:43:00 +0900

    no message

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 require("dotenv").config();
+const app = require("./package.json");
 const puppeteer = require("puppeteer");
 const deviceDescriptors = require("puppeteer/DeviceDescriptors");
 const colors = require("colors");
@@ -7,52 +8,147 @@ const delay = require("delay");
 
 const input = process.argv[2];
 
-const time = 2000;
-
 const options = {
-  useAgent: "",
-  device: deviceDescriptors["iPhone X"]
+  device: deviceDescriptors[eval(Math.floor(Math.random() * deviceDescriptors.length - 1))]
+};
+
+function random(min, max) { 
+  return Math.floor(Math.random() * (max + 1 - min)) + min;
+};
+
+let wait = {
+  min: 10000,
+  max: 60000
+};
+let delayTime = random(wait.min, wait.max); 
+let completeTime = 0;
+let currentURL = null;
+
+/*
+====================
+■ mysite
+====================
+*/
+let mysite = {
+  origin: process.env.MYSITE_ORIGIN
 };
 
 /*
 ====================
-■ eroterest
+■ target
 ====================
 */
-let eroterest = {
-  origin: "https://movie.eroterest.net/"
-};
-
-/*
-====================
-■ erojiru
-====================
-*/
-let erojiru = {
-  origin: "https://erojiru.net/"
+let target = {
+  origin: process.env.TARGET_ORIGIN,
+  matchLink: process.env.TARGET_MATCH_LINK
 };
 
 async function run() {
-  console.log("bot running!!".red);
   const browser = await puppeteer.launch({
   	headless: false,
-  	slowMo: 30,
+  	slowMo: 50,
     defaultViewport: null,
     args: [
       `--window-size=${process.env.WIDTH},${process.env.HEIGHT}`,
       "--window-position=0,0",
       "--no-sandbox",
+      '--disable-infobars', //情報バー非表示
+      '--incognito', //シークレットモード
       "--proxy-pac-url=http://www.cybersyndrome.net/pac.cgi?rl=a&a=a&rd=r&ru=a",
-      //"--proxy-server=110.78.81.75:8080",
+      //"--proxy-server=",
       //"--no-referrers"
     ],
-    //devtools: true
+    devtools: true
+  });
+  //debug
+  console.log(colors.green(
+  `===============================================================================================\n`+
+  `AppName: ${app.name}                                                                           \n`+
+  `Version: ${app.version}                                                                        \n`+
+  `Description: ${app.description}                                                                \n`+
+  `===============================================================================================\n`
+  ));
+  const page = (await browser.pages())[0];
+  await page.emulate(options.device);
+
+  async function autoScroll(page) {
+    await page.evaluate(async function() {
+      let totalHeight = 0;
+      let distance = window.innerHeight;
+      let timer = setInterval(function() {
+        let scrollHeight = document.body.scrollHeight
+        window.scrollBy(0, distance);
+        totalHeight += distance;
+        if(totalHeight >= scrollHeight){
+          clearInterval(timer);
+          resolve();
+        }
+      }, 100)
+    });
+  }
+
+  await page.goto(mysite.origin, {
+      timeout: 0,
+      waitUntil: "domcontentloaded"
+  });
+  //wait
+  await delay(wait.min, wait.max);
+  //scroll
+  autoScroll(page);
+
+  currentURL = page.url();
+  //リファラーを追加
+  await page.setExtraHTTPHeaders({
+      "referer": `${currentURL}`
   });
 
-  const page = await browser.newPage();
-  await page.setUserAgent(options.useAgent);
-  await page.emulate(options.device);
-  await page.goto(erojiru.origin);
-  //await browser.close()
+  await page.goto(target.origin, {
+    timeout: 0,
+    waitUntil: "domcontentloaded"
+  });
+
+  let patrol = random(eval(process.env.PATROL_MIN), eval(process.env.PATROL_MAX));
+  let count = 1;
+  while(count <= patrol) {
+    delayTime = random(wait.min, wait.max);
+    completeTime += delayTime;
+    currentURL = page.url();
+    //debug
+    console.log(`${count}ページ目`.red);
+    console.log("現在のURL: ".blue + colors.underline.blue(currentURL));
+    console.log(`待ち時間: ${delayTime / 1000}秒`.red);
+    //wait
+    await delay(delayTime);
+    //scroll
+    autoScroll(page);
+    let anchor = await page.$$(`a[href*="${target.matchLink}"]`);
+    let linkLength = anchor.length;
+    //debug
+    console.log(`ページ条件にマッチしたリンク数: ${linkLength}`.red);
+    //リンクをランダムに決定
+    let targetIndex = random(1, linkLength);
+    //debug
+    console.log(`アンカーリンクのインデックス番号: ${targetIndex}`.red);
+    let gotoLink = await (await anchor[targetIndex - 1].getProperty("href")).jsonValue();
+    //debug
+    console.log("遷移先URL: ".blue + colors.underline.blue(gotoLink));
+    //リファラーを追加
+    await page.setExtraHTTPHeaders({
+      "referer": `${currentURL}`
+    });
+    await page.goto(gotoLink, {
+      timeout: 0,
+      waitUntil: "domcontentloaded"
+    });
+    //debug
+    console.log("done!\n".green);
+    //next
+    count += 1;
+  }
+  //debug
+  console.log(`ターゲットページの総閲覧数: ${patrol}回`.red);
+  console.log(`総滞在時間: ${completeTime / 1000}秒`.red);
+  console.log("complete!!".green);
+  await browser.close();
 }
 run();
