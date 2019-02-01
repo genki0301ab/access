686a7eacb66b6425a7ef2a19c89fcbd86d614067
岡田元輝 <genki0301ab@gmail.com>
2019-02-01 21:35:27 +0900

    ipスプーフィング以外完成

--- app.js
+++ app.js
@@ -16,21 +16,14 @@ function random(min, max) {
   return Math.floor(Math.random() * (max + 1 - min)) + min;
 };
 
-let wait = {
-  min: 10000,
-  max: 60000
-};
-let delayTime = random(wait.min, wait.max); 
-let completeTime = 0;
-let currentURL = null;
-
 /*
 ====================
 ■ mysite
 ====================
 */
 let mysite = {
-  origin: process.env.MYSITE_ORIGIN
+  origin: process.env.MYSITE_ORIGIN,
+  matchLink: process.env.MYSITE_MATCH_LINK
 };
 
 /*
@@ -43,6 +36,27 @@ let target = {
   matchLink: process.env.TARGET_MATCH_LINK
 };
 
+let wait = {
+  min: 10000,
+  max: 60000
+};
+let delayTime = random(wait.min, wait.max); 
+let completeTime = 0;
+
+//条件にマッチしたアンカーリンク
+let anchor = null;
+//マッチしたリンクの数
+let linkLength = null;
+//リンクを取得したインデックス番号
+let targetIndex = null;
+//遷移先URL
+let gotoLink = null;
+
+//巡回回数
+let patrol = random(eval(process.env.PATROL_MIN), eval(process.env.PATROL_MAX));
+//巡回回数カウント
+let count = 1;
+
 async function run() {
   const browser = await puppeteer.launch({
   	headless: false,
@@ -54,13 +68,12 @@ async function run() {
       "--no-sandbox",
       '--disable-infobars', //情報バー非表示
       '--incognito', //シークレットモード
-      "--proxy-pac-url=http://www.cybersyndrome.net/pac.cgi?rl=a&a=a&rd=r&ru=a",
+      //"--proxy-pac-url=",
       //"--proxy-server=",
       //"--no-referrers"
     ],
     devtools: true
   });
-  //debug
   console.log(colors.green(
   `===============================================================================================\n`+
   `AppName: ${app.name}                                                                           \n`+
@@ -71,6 +84,7 @@ async function run() {
   const page = (await browser.pages())[0];
   await page.emulate(options.device);
 
+  //スクロール処理
   async function autoScroll(page) {
     await page.evaluate(async function() {
       let totalHeight = 0;
@@ -83,72 +97,145 @@ async function run() {
           clearInterval(timer);
           resolve();
         }
-      }, 100)
+      }, 100);
     });
   }
 
-  await page.goto(mysite.origin, {
+  //ページ遷移処理
+  async function access(page, requestURL, referer) {
+    await page.setExtraHTTPHeaders({
+      "referer": referer
+    });
+    await page.goto(requestURL, {
       timeout: 0,
       waitUntil: "domcontentloaded"
-  });
-  //wait
+    });
+  }
+
+  async function patrol(page, ) {
+  }
+
+  await access(page, mysite.origin, "https://google.com");
+
+  //debug
+  console.log("参照サイトにアクセス".red);
+  console.log("現在のURL: ".blue + colors.underline.blue(page.url()) + "\n");
+
   await delay(wait.min, wait.max);
-  //scroll
-  autoScroll(page);
+  await autoScroll(page);
 
-  currentURL = page.url();
-  //リファラーを追加
-  await page.setExtraHTTPHeaders({
-      "referer": `${currentURL}`
-  });
-
-  await page.goto(target.origin, {
-    timeout: 0,
-    waitUntil: "domcontentloaded"
-  });
-
-  let patrol = random(eval(process.env.PATROL_MIN), eval(process.env.PATROL_MAX));
-  let count = 1;
+  //参照サイトを巡回、変数リセット
+  patrol = random(eval(process.env.MYSITE_PATROL_MIN), eval(process.env.MYSITE_PATROL_MAX));
+  count = 1;
+  completeTime = 0;
   while(count <= patrol) {
     delayTime = random(wait.min, wait.max);
     completeTime += delayTime;
-    currentURL = page.url();
+
     //debug
     console.log(`${count}ページ目`.red);
-    console.log("現在のURL: ".blue + colors.underline.blue(currentURL));
+    console.log("現在のURL: ".blue + colors.underline.blue(page.url()));
     console.log(`待ち時間: ${delayTime / 1000}秒`.red);
-    //wait
+
     await delay(delayTime);
-    //scroll
-    autoScroll(page);
-    let anchor = await page.$$(`a[href*="${target.matchLink}"]`);
-    let linkLength = anchor.length;
+    await autoScroll(page);
+    anchor = await page.$$(`a[href*="${mysite.matchLink}"]`);
+    linkLength = anchor.length;
+
     //debug
     console.log(`ページ条件にマッチしたリンク数: ${linkLength}`.red);
+
     //リンクをランダムに決定
-    let targetIndex = random(1, linkLength);
+    targetIndex = random(1, linkLength);
+
     //debug
-    console.log(`アンカーリンクのインデックス番号: ${targetIndex}`.red);
-    let gotoLink = await (await anchor[targetIndex - 1].getProperty("href")).jsonValue();
+    console.log(`リンクを取得したインデックス番号: ${targetIndex}`.red);
+
+    gotoLink = await (await anchor[targetIndex - 1].getProperty("href")).jsonValue();
+
     //debug
     console.log("遷移先URL: ".blue + colors.underline.blue(gotoLink));
-    //リファラーを追加
-    await page.setExtraHTTPHeaders({
-      "referer": `${currentURL}`
-    });
-    await page.goto(gotoLink, {
-      timeout: 0,
-      waitUntil: "domcontentloaded"
-    });
+
+    await access(page, gotoLink, page.url());
+
     //debug
     console.log("done!\n".green);
+
     //next
     count += 1;
   }
+
   //debug
-  console.log(`ターゲットページの総閲覧数: ${patrol}回`.red);
-  console.log(`総滞在時間: ${completeTime / 1000}秒`.red);
+  console.log(`参照サイトの総閲覧数: ${patrol}回`.red);
+  console.log(`参照サイトの総滞在時間: ${completeTime / 1000}秒`.red);
+  console.log("complete!!\n".green);
+
+  //**************************************************
+
+  //ターゲットにアクセス
+  anchor = await page.$$(`a[href*="${target.origin}"]`);
+  linkLength = anchor.length;
+  if(linkLength >= 1) {
+    targetIndex = random(1, linkLength);
+    gotoLink = await (await anchor[targetIndex - 1].getProperty("href")).jsonValue();
+    await access(page, gotoLink, page.url());
+  }
+  if(linkLength == 0) {
+    await access(page, target.origin, page.url());
+  }
+
+  //debug
+  console.log("ターゲットサイトにアクセス".red);
+  console.log("現在のURL: ".blue + colors.underline.blue(page.url()) + "\n");
+
+  //**************************************************
+  
+  //ターゲットサイトを巡回、変数リセット
+  patrol = random(eval(process.env.TARGET_PATROL_MIN), eval(process.env.TARGET_PATROL_MAX));
+  count = 1;
+  completeTime = 0;
+  while(count <= patrol) {
+    delayTime = random(wait.min, wait.max);
+    completeTime += delayTime;
+
+    //debug
+    console.log(`${count}ページ目`.red);
+    console.log("現在のURL: ".blue + colors.underline.blue(page.url()));
+    console.log(`待ち時間: ${delayTime / 1000}秒`.red);
+
+    await delay(delayTime);
+    await autoScroll(page);
+    anchor = await page.$$(`a[href*="${target.matchLink}"]`);
+    linkLength = anchor.length;
+
+    //debug
+    console.log(`ページ条件にマッチしたリンク数: ${linkLength}`.red);
+
+    //リンクをランダムに決定
+    targetIndex = random(1, linkLength);
+
+    //debug
+    console.log(`リンクを取得したインデックス番号: ${targetIndex}`.red);
+
+    gotoLink = await (await anchor[targetIndex - 1].getProperty("href")).jsonValue();
+
+    //debug
+    console.log("遷移先URL: ".blue + colors.underline.blue(gotoLink));
+
+    await access(page, gotoLink, page.url());
+
+    //debug
+    console.log("done!\n".green);
+
+    //next
+    count += 1;
+  }
+
+  //debug
+  console.log(`ターゲットサイトの総閲覧数: ${patrol}回`.red);
+  console.log(`ターゲットサイトの総滞在時間: ${completeTime / 1000}秒`.red);
   console.log("complete!!".green);
+
   await browser.close();
 }
 run();
